35934d9871386a9b4da4a149c0aecc219988346a
Pavel <pl.hancharou@gmail.com>
2021-01-25 18:18:49 +0300

    initial

--- public/app.js
+++ public/app.js
@@ -1,48 +1,49 @@
-const toCurrency = price => {
-  return new Intl.NumberFormat('en-Us', {
-    currency: 'USD',
-    style: 'currency'
-  }).format(price);
-};
+document.addEventListener('DOMContentLoaded', function () {
+  const toCurrency = price => {
+    return new Intl.NumberFormat('en-Us', {
+      currency: 'USD',
+      style: 'currency'
+    }).format(price);
+  };
 
-const toDate = date => {
-  return new Intl.DateTimeFormat('en-GB', {
-    day: '2-digit',
-    month: 'long',
-    year: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit'
-  }).format(new Date(date));
-};
+  const toDate = date => {
+    return new Intl.DateTimeFormat('en-GB', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit'
+    }).format(new Date(date));
+  };
 
-document.querySelectorAll('.date').forEach(node => {
-  node.textContent = toDate(node.textContent);
-});
+  document.querySelectorAll('.date').forEach(node => {
+    node.textContent = toDate(node.textContent);
+  });
 
-document.querySelectorAll('.price').forEach(node => {
-  node.textContent = toCurrency(node.textContent);
-});
+  document.querySelectorAll('.price').forEach(node => {
+    node.textContent = toCurrency(node.textContent);
+  });
 
-const $cart = document.querySelector('#cart');
+  const $cart = document.querySelector('#cart');
 
-if ($cart) {
-  $cart.addEventListener('click', event => {
-    if (event.target.classList.contains('js-remove')) {
-      const id = event.target.dataset.id;
-      const csrf = event.target.dataset.csrf;
+  if ($cart) {
+    $cart.addEventListener('click', event => {
+      if (event.target.classList.contains('js-remove')) {
+        const id = event.target.dataset.id;
+        const csrf = event.target.dataset.csrf;
 
-      fetch('/cart/remove/' + id, {
-        method: 'delete',
-        headers: {
-          'X-XSRF-TOKEN': csrf
-        }
-      })
-        .then(res => res.json())
-        .then(cart => {
-          if (cart.courses.length) {
-            const html = cart.courses.map(c => {
-              return `
+        fetch('/cart/remove/' + id, {
+          method: 'delete',
+          headers: {
+            'X-XSRF-TOKEN': csrf
+          }
+        })
+          .then(res => res.json())
+          .then(cart => {
+            if (cart.courses.length) {
+              const html = cart.courses.map(c => {
+                return `
                    <tr>
                     <td>${c.title}</td>
                     <td>${c.count}</td>
@@ -51,15 +52,17 @@ if ($cart) {
                     </td >
                   </tr >
               `;
-            }).join('');
-            $cart.querySelector('tbody').innerHTML = html;
-            $cart.querySelector('.price').textContent = toCurrency(cart.price);
-          } else {
-            $cart.innerHTML = `<p>The cart is empty</p> `;
-          }
-        });
-    }
-  });
-}
+              }).join('');
+              $cart.querySelector('tbody').innerHTML = html;
+              $cart.querySelector('.price').textContent = toCurrency(cart.price);
+            } else {
+              $cart.innerHTML = `<p>The cart is empty</p> `;
+            }
+          });
+      }
+    });
+  }
 
-M.Tabs.init(document.querySelectorAll('.tabs'));
+  M.Tabs.init(document.querySelectorAll('.tabs'));
+  M.Sidenav.init(document.querySelectorAll('.sidenav'));
+});
